193dd4ece1d3ddfaf7445726812843cf445d1c22
James Finlay <jtfinlay@ualberta.ca>
2017-12-29 15:16:30 -0800

    Sidebar layout fix

--- src/components/UserProjectsBar/UserProjectsBar.tsx
+++ src/components/UserProjectsBar/UserProjectsBar.tsx
@@ -102,26 +102,24 @@ export class UserProjectsBar extends React.Component <IUserProjectsBarProps, IUs
 	render() {
 		const projects = this.state.projects || [];
 		return (
-			<div ref="modal">
-				<div className="user-projects">
-					{this.state.loading && <div className="loading" />}
-					{!this.state.loading && this.state.projects === null && 
-						<div className="error">
-						<p>An error occured loading user projects. Please reload and try again.</p>
-					</div>}
-					<ul>
-						{projects.map(project => (
-							<li 
-								key={project.fullName}
-								className={classNames({ selected: this.state.selectedProject === project.fullName })}
-								onClick={() => this.selectProject(project)}
-							>{project.fullName}
-							</li>
-						))}
-					</ul>
-					<div className="add-project">
-						<button onClick={() => this.openModal()}>Add Project</button>
-					</div>
+			<div className="user-projects">
+				{this.state.loading && <div className="loading" />}
+				{!this.state.loading && this.state.projects === null && 
+					<div className="error">
+					<p>An error occured loading user projects. Please reload and try again.</p>
+				</div>}
+				<ul>
+					{projects.map(project => (
+						<li 
+							key={project.fullName}
+							className={classNames({ selected: this.state.selectedProject === project.fullName })}
+							onClick={() => this.selectProject(project)}
+						>{project.fullName}
+						</li>
+					))}
+				</ul>
+				<div className="add-project">
+					<button onClick={() => this.openModal()}>Add Project</button>
 				</div>
 				<Modal
 					className="add-project-modal"
