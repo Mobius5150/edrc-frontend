e6915c1a9088fa988d3b5fd7e6ce93411748cc15
Mobius5150 <mike@michaelblouin.ca>
2018-01-13 20:24:17 -0800

    Add build status embed modal

--- src/components/BuildStatusControl/BuildStatusControl.tsx
+++ src/components/BuildStatusControl/BuildStatusControl.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import * as Modal from 'react-modal';
+import * as url from 'url';
 import classNames from 'classnames';
 import './style.css';
 
 interface IBuildStatusControlState {
+	embedModalOpen: boolean;
+	compactChecked: boolean;
 }
 
 interface IBuildStatusControlProps {
@@ -18,57 +22,161 @@ interface IBuildStatusControlProps {
 }
 
 export class BuildStatusControl extends React.Component <IBuildStatusControlProps, IBuildStatusControlState> {
+	private static readonly embedPreviewFormats = {
+		markdown: 'Markdown',
+		html: 'HTML',
+		asciidoc: 'AsciiDoc',
+		url: 'Url'
+	};
+
+	private static embedPreviewFormat: string = 'markdown';
 	private url: string;
 
 	public constructor(props: IBuildStatusControlProps) {
 		super(props);
+
+		this.state = {
+			embedModalOpen: false,
+			compactChecked: !!this.props.compact,
+		};
 	}
 
 	componentWillMount() {
-		this.updateUrl(this.props);
+		this.url = this.getStatusImageUrl(this.props);
 	}
 
-	componentWillUpdate(newProps: IBuildStatusControlProps) {
-		this.updateUrl(newProps);
+	componentWillReceiveProps(newProps: IBuildStatusControlProps) {
+		this.url = this.getStatusImageUrl(newProps);
 	}
 
-	private updateUrl(props: IBuildStatusControlProps) {
+	private getStatusImageUrl(props: IBuildStatusControlProps) {
+		let generatedUrl = '';
 		let hasQuery = false;
 		if (props.buildId) {
-			this.url = `/api/v1/user/${props.owner}/project/${props.project}/build/${props.buildId}/img/status`;
+			generatedUrl = `/api/v1/user/${props.owner}/project/${props.project}/build/${props.buildId}/img/status`;
 		} else {
-			this.url = `/api/v1/user/${props.owner}/project/${props.project}/img/status`;
+			generatedUrl = `/api/v1/user/${props.owner}/project/${props.project}/img/status`;
 			if (props.ref) {
 				hasQuery = true;
-				this.url = this.url + '?ref=' + encodeURIComponent(props.ref);
+				generatedUrl = generatedUrl + '?ref=' + encodeURIComponent(props.ref);
 			} else if (props.branch) {
 				hasQuery = true;
-				this.url = this.url + '?branch=' + encodeURIComponent(props.branch);
+				generatedUrl = generatedUrl + '?branch=' + encodeURIComponent(props.branch);
 			}
 		}
 
 		if (props.compact) {
 			if (hasQuery) {
-				this.url = `${this.url}&compact=true`;
+				generatedUrl = `${generatedUrl}&compact=true`;
 			} else {
-				this.url = `${this.url}/?compact=true`;
+				generatedUrl = `${generatedUrl}?compact=true`;
 			}
 		}
+
+		return generatedUrl;
 	}
 
 	render() {
 		const embed = !!this.props.enableEmbed;
 		const embedText = embed && !!this.props.enableEmbedText;
 		const hoverText = this.props.hoverText || (embed ? 'embed status' : '');
-		return (
-			<div
-				className={classNames({'embed-link': embed, 'build-status': true})}
-				title={hoverText}
-			>
-				<img src={this.url} />
-				{embedText && <span className="embed-text">embed status</span>}
-			</div>
+		const imageUrl = this.state.embedModalOpen && url.resolve(
+			window.location.href,
+			this.getStatusImageUrl({
+				...(this.props as IBuildStatusControlProps),
+				compact: this.state.compactChecked
+			})
 		);
+
+		return (
+			<React.Fragment>
+				<div
+					className={classNames({'embed-link': embed, 'build-status': true})}
+					title={hoverText}
+					onClick={(e) => this.openModal(e)}
+				>
+					<img src={this.url} />
+					{embedText && <span className="embed-text">embed status</span>}
+					<div onClick={e => e.stopPropagation()}>
+						{embed && this.state.embedModalOpen &&
+							<Modal
+								className="embed-status-modal"
+								isOpen={this.state.embedModalOpen}
+								onRequestClose={() => this.closeModal()}
+								contentLabel="Embed Build Status"
+							>
+								<h1>Embed Build Status</h1>
+								<div>
+									Format:&nbsp;
+									<select onChange={(e) => this.changeEmbedPreviewFormat(e)}>
+										{Object.keys(BuildStatusControl.embedPreviewFormats).map(id => (
+											<option key={id} value={id} selected={BuildStatusControl.embedPreviewFormat === id}>
+												{BuildStatusControl.embedPreviewFormats[id]}
+											</option>
+										))}
+									</select>
+								</div>
+								<div>
+									<label>Compact: <input type="checkbox" onChange={e => this.changeEmbedPreviewCompact(e)} checked={this.state.compactChecked} /></label>
+								</div>
+								<div className="embed-preview">
+									Preview: <div className="build-status"><img src={imageUrl as string} /></div>
+								</div>
+								<div className="embed-source">
+									<pre>{this.generateEmbedPreview(imageUrl as string)}</pre>
+								</div>
+								{/* TODO: Add a link to the docs on embedding */}
+								<button onClick={() => this.closeModal()}>close</button>
+							</Modal>}
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+
+	private openModal(e: React.MouseEvent<any>) {
+		if (this.props.enableEmbed && !this.state.embedModalOpen) {
+			if (e && e.stopPropagation()) {
+				e.stopPropagation();
+			}
+
+			this.setState({...this.state, embedModalOpen: true});
+		}
+	}
+
+	private closeModal() {
+		this.setState({...this.state, embedModalOpen: false});
+	}
+
+	private changeEmbedPreviewFormat(e: React.ChangeEvent<HTMLSelectElement>) {
+		if (e && e.target) {
+			BuildStatusControl.embedPreviewFormat = e.target.value;
+			this.setState({...this.state});
+		}
+	}
+
+	private changeEmbedPreviewCompact(e: React.ChangeEvent<HTMLInputElement>) {
+		if (e && e.target) {
+			this.setState({...this.state, compactChecked: e.target.checked});
+		}
+	}
+
+	private generateEmbedPreview(imageUrl: string): string {
+		const title = 'design rule check status';
+		const linkUrl = url.resolve(window.location.href, `/g/${this.props.owner}/${this.props.project}`);
+		switch (BuildStatusControl.embedPreviewFormat) {
+			case 'html':
+				return `<a href="${linkUrl}">
+	<img alt="${title}" src="${imageUrl}" />
+</a>`;
+			case 'url':
+				return imageUrl;
+			case 'asciidoc':
+				return `image:${imageUrl}[link="${linkUrl}",title="${title}"]`;
+			case 'markdown':
+			default:
+				return `[![${title}](${imageUrl})](${linkUrl})`;
+		}
 	}
 }
 
