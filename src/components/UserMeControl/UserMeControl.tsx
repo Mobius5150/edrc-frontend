c58cfe623379702d2008297adfa72b7bb8ec17e4
Mobius5150 <mike@michaelblouin.ca>
2018-01-15 12:14:56 -0800

    Add frontend version indicator to 'me' control

--- src/components/UserMeControl/UserMeControl.tsx
+++ src/components/UserMeControl/UserMeControl.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import classNames from 'classnames';
-
 import { Link } from 'react-router-dom';
 import { UserController, IUser } from '../../controllers/User';
+import { resolve as urlResolve } from 'url';
 import './style.css';
 
 interface IUserMeControlState {
@@ -12,7 +12,9 @@ interface IUserMeControlState {
 }
 
 export class UserMeControl extends React.Component <any, IUserMeControlState> {
+	private static readonly FrontendCookieName = '__edrc_frontend';
 	private userController: UserController;
+	private frontendVer: string | null;
 
 	public constructor(props: any) {
 		super(props);
@@ -32,6 +34,8 @@ export class UserMeControl extends React.Component <any, IUserMeControlState> {
 			.catch(e => {
 				this.setState({...this.state, signedIn: false});
 			});
+
+		this.frontendVer = this.getFrontendCookieVal();
 	}
 
 	toggleMenuDropdown() {
@@ -52,6 +56,22 @@ export class UserMeControl extends React.Component <any, IUserMeControlState> {
 				<ul>
 					<li className="menu-header">Signed in as <b>{userName}</b></li>
 					<li className="menu-divider"/>
+					{null !== this.frontendVer &&
+						<React.Fragment>
+							<li className="menu-header frontend-version" title="Active frontend version">
+								<span className="ion-ios-flask frontend-version-icon" />
+								<a
+									href={urlResolve(window.location.href, '?clearFrontend=true')}
+									className="clear-frontend"
+									title="Clear active frontend"
+								>
+									<span className="ion-close" />
+								</a>
+								{this.frontendVer}
+							</li>
+							<li className="menu-divider"/>
+						</React.Fragment>
+					}
 					<li className="menu-item">Settings</li>
 					<li className="menu-item">Help</li>
 					<Link to="/logout" target="_self">
@@ -78,6 +98,24 @@ export class UserMeControl extends React.Component <any, IUserMeControlState> {
 				}
 			</div>
 		);
+	}
+
+	private getFrontendCookieVal(): string | null {
+		if (document) {
+			const cookies = document.cookie.split(';');
+			for (const c in cookies) {
+				const [key, value] = cookies[c].split('=', 2);
+				if (!key || !value) {
+					continue;
+				}
+
+				if (key === UserMeControl.FrontendCookieName) {
+					return value;
+				}
+			}
+		}
+
+		return null;
 	}
 }
 
